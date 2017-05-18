30fc6e1a3034db361e34e8d855c758b8fbe6258b
Ashley Williams <ashley666ashley@gmail.com>
2017-05-18 12:18:31 -0400

    feat(lint): add standard

--- index.js
+++ index.js
@@ -5,44 +5,39 @@
 
 module.exports = tarpit
 
-function tarpit(opts){
+function tarpit (opts) {
+  opts.wait = opts.wait || 10
+  opts.max = opts.max || 10000
 
-  opts.wait = opts.wait||10
-  opts.max = opts.max||10000
-
-  return function(key,cb){
-    opts.get(key,function(err,obj){
-
+  return function (key, cb) {
+    opts.get(key, function (err, obj) {
       var now = Date.now()
-      obj.time = obj.time||0
-      if(!obj.count || isNaN(obj.count)) {
+      obj.time = obj.time || 0
+      if (!obj.count || isNaN(obj.count)) {
         obj.count = 0
       }
       // it should take no requests for opts.max*2 to escape from the tarpit.
-      if(obj.time < now-(opts.max*2)) {
+      if (obj.time < now - (opts.max * 2)) {
         obj.count = 0
       }
 
-      var delay = Math.pow(opts.wait,obj.count)
+      var delay = Math.pow(opts.wait, obj.count)
 
-      if(delay > opts.max) {
+      if (delay > opts.max) {
         delay = opts.max
       }
 
-      setTimeout(function(){
-        cb(err,delay)
-      },delay||0)
+      setTimeout(function () {
+        cb(err, delay)
+      }, delay || 0)
 
       return delay
     })
   }
 }
 
-function json(o){
-  try{
+function json (o) {
+  try {
     return JSON.parse(o)
-  } catch(e) {}
+  } catch (e) {}
 }
-
-
-
